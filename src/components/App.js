ba5f8a6865efc7b92ca89d56f336a39c8daadc07
Amanda Smith <ajs.amandasmith@gmail.com>
2022-06-07 17:14:35 -0500

    commit tree 751e2875b0b7ce171ac2d61abd1b92a7acdb7f4c

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setDogImage(data.message);
+        setIsLoaded(true);
+      })
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img alt="A Random Dog" src={dogImage}></img>
+    </div>
+  )}
+
+export default App;
